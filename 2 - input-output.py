07b93e24d3d70ea9a9730f0081770d15d9bdf54d
leandro-gois <leandroag2007@gmail.com>
2025-10-18 23:00:36 -0300

    Simple Input and Output in Python => input-output.py

---
+++ 2 - input-output.py
@@ -0,0 +1,9 @@
+# Simple Input and Output in Python
+
+# Get user information
+name = input("What's your name? ")
+age = int(input("What's your age? "))
+city = input("What city are you from? ")
+
+print(f"\nHello {name}!")
+print(f"You're {age} years old and from {city}.")
