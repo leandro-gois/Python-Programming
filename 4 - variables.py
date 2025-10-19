ff3b3e3a15d8189109ce7ea69bf612510dba17e2
leandro-gois <leandroag2007@gmail.com>
2025-10-19 07:21:30 -0300

    Basic Variable Assignment  => variables.py

---
+++ 4 - variables.py
@@ -0,0 +1,10 @@
+# Creating variables with the assignment operator
+name = "Leandro"
+age = 30
+height = 5.8
+is_student = True
+
+print(f"Name: {name}")
+print(f"Age: {age}")
+print(f"Height: {height} feet")
+print(f"Student: {is_student}")
